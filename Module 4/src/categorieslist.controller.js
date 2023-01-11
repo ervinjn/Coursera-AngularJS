d6dd8925d39ad14a84e75bbf574312b06505a932
ervinjn <84634490+ervinjn@users.noreply.github.com>
2023-01-11 15:12:53 -0600

    Update categorieslist.controller.js

--- Module 4/src/categorieslist.controller.js
+++ Module 4/src/categorieslist.controller.js
@@ -10,8 +10,6 @@ function CategoriesListController(items) {
   var CLctrl = this;
   console.log(items.data)
   CLctrl.items = items.data
-  for(let i=0, i < CLctrl.items.length, i++) {
-    console.log(CLctrl.items[i].name)
   }
   
 })();
