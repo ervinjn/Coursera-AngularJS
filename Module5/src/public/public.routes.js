566f9ec68367dacd7b32cf9befd2c2326d1a68ae
Ervin <jervin@MDOT.MS.GOV>
2023-01-27 16:09:57 -0600

    made a change to userservice to see if that helps.

--- Module5/src/public/public.routes.js
+++ Module5/src/public/public.routes.js
@@ -45,7 +45,7 @@ function routeConfig ($stateProvider) {
       url: '/signup',
       templateUrl: 'src/public/signup/signup.html',
       controller: 'FormController',
-      controllerAs: 'formCtrl'
+      controllerAs: 'formCtrl',
       resolve: {
         test: [ function () { return true; }]}
     });
