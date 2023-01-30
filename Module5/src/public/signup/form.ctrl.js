f73abaad7d0e6c982906f0c8407e8a8d8e82eb7a
Ervin <jervin@MDOT.MS.GOV>
2023-01-30 14:00:26 -0600

    working on the form

--- Module5/src/public/signup/form.ctrl.js
+++ Module5/src/public/signup/form.ctrl.js
@@ -21,7 +21,7 @@ function FormController(UserService,MenuService) {
   // Build an array of valid favdish codes
   ctrl.codes = [];
   var itemData = MenuService.getAllMenuItems();
-  console.log(itemData)
+  console.log(itemData.$$state.value.data)
   let categories = Object.values(itemData)
   console.log(categories)
 
