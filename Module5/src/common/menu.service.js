31a25cd0a8fec01b6ea6632247cdd253e5ce58eb
Jason <ervinjn@gmail.com>
2023-01-30 17:50:32 -0600

    having some trouble with accessing the data returned from http request

--- Module5/src/common/menu.service.js
+++ Module5/src/common/menu.service.js
@@ -35,7 +35,7 @@ function MenuService($http, ApiPath) {
 
 service.getAllMenuItems = function() {
     return $http.get('data/menu.json').then( function(response) { 
-        console.log('response.data: '+ response.data)
+        console.log(response.data)
         return response.data }); 
 };
 
