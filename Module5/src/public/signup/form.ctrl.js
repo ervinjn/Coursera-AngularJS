b76c0dcb45cd490337598a0b9dce5e47687d5f01
Ervin <jervin@MDOT.MS.GOV>
2023-01-30 13:05:30 -0600

    fixed some typos

--- Module5/src/public/signup/form.ctrl.js
+++ Module5/src/public/signup/form.ctrl.js
@@ -24,10 +24,10 @@ function FormController(UserService,MenuService) {
   for(let prop in itemData) {
       for(let i=0; i < prop.menu_items.length; i++) { codes.push(prop.menu_items[i].short_name) } }
     console.log(ctrl.codes)
-  }
 
- ctrl.stored = false
-ctrl.store = function() { 
+
+ctrl.stored = false
+  ctrl.store = function() { 
 
     // first task, before storage, is to check whether they have entered a valid favdish code
     if (ctrl.codes.indexOf(ctrl.user.favdishcode) == -1){
@@ -38,11 +38,8 @@ ctrl.store = function() {
       return ctrl.stored}
    }
 
-
-
-}
-
-
+  }
+ 
 })();
 
 
